5957ec2c654406896d6eb3b09628a15c9ff87cf6
John Moon <ymoon95@gmail.com>
2016-03-21 11:28:51 -0400

    Added code for GET Requests to set link

---
+++ sample/search.py
@@ -0,0 +1,42 @@
+
+#!/usr/bin/python
+
+#-----------------------------------------------------------------------
+# twitter-search
+#  - performs a basic keyword search for tweets containing the keywords
+#    "lazy" and "dog"
+#-----------------------------------------------------------------------
+import json
+from twitter import *
+
+#-----------------------------------------------------------------------
+# load our API credentials 
+#-----------------------------------------------------------------------
+config = {}
+execfile("config.py", config)
+
+#-----------------------------------------------------------------------
+# create twitter API object
+#-----------------------------------------------------------------------
+twitter = Twitter(
+		        auth = OAuth(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"]))
+
+
+#-----------------------------------------------------------------------
+# perform a basic search 
+# Twitter API docs:
+# https://dev.twitter.com/docs/api/1/get/search
+#-----------------------------------------------------------------------
+query = twitter.search.tweets(q = "usa", geocode = "37.781157,-122.398720,250mi", result_type = "recent", include_entities = "false")
+
+#-----------------------------------------------------------------------
+# How long did this query take?
+#-----------------------------------------------------------------------
+print "Search complete (%.3f seconds)" % (query["search_metadata"]["completed_in"])
+print json.dumps(query, indent=2)
+
+#-----------------------------------------------------------------------
+# Loop through each of the results, and print its content.
+#-----------------------------------------------------------------------
+# for result in query["statuses"]:
+# 	print "(%s) @%s %s" % (result["created_at"], result["user"]["screen_name"], result["text"])
--- twitter_api.py
+++ twitter_api.py
@@ -1,12 +1,16 @@
 from flask import *
 from extensions import mysql
 
+
+dani = "https://api.twitter.com/1.1/trends/place.json?id=1"
 link = "https://api.twitter.com/1.1/search/tweets.json?q="
+hashtag = "%23"
+CONSUMER_KEY = "h2Ky8wu3DuBit9b86cgeuPNJd"
+CONSUMER_SECRET = "7xP8Y4VA5WwH8anlIEZwlBKzu4N7bmRtfTa0EMu4jroKJePXfO"
+
 
 twitter_api = Blueprint('twitter_api', __name__, template_folder='templates')
 
 @twitter_api.route('/api/v1/twitter/<string:query>', methods=['GET'])
 def twitter_api_route(query):
 	if request.method == 'GET':
-
-
