75d5e4a47e5bf964c817a236e50228cd56921cd9
arenvogel <amvamax@umich.edu>
2016-04-08 17:04:56 -0400

    made some fixes

--- Classifier python files/naive_system.py
+++ Classifier python files/naive_system.py
@@ -4,11 +4,10 @@ from nltk.tokenize import word_tokenize
 import nltk
 import json
 
-class TweetInfo:
-	def __init__(self, sentiment, location):
-		self.sent = sentiment
-		self.loc = location
 
+#positive and negative tweet counts - specific to a location query that came in 
+pos = 0
+neg = 0
 
 # 4) Read from that file and store back into structure that it originally was 
 		#- MAKE SURE STRUCTURE IS CORRECT
@@ -21,8 +20,6 @@ nb_classifier = NBC.train(training)
 # 6) Run api request to get tweets form users containing our specified political words
 	#- FILL THIS IN - MIGHT BE BEST TO READ THIS IN FROM A FILE AS WELL
 
-#dictionary to store tweets (DO TWEETS HAVE IDS) mapped to a struct that holds all the data we will need about each tweet (location and sentiment, etc)
-tweets_sentiment = {}
 
 # 7) Grab the content of each tweet and format into testable data - like how we formatted training data
 	#- NOT SURE WHAT TWEET DATA (test_sentences) WILL LOOK LIKE BUT LOOP THRU IT AND RUN THE FOLLOWING CODE TO FORMAT IT
@@ -34,20 +31,16 @@ tweets_sentiment = {}
 
 	#8) run classification on each tweet and receive its sentiment 
 		#NEED TO SET UP tweet_id VALUE AND location VALUE
-	curr_tweet_info = TweetInfo(nb_classifier.classify(test_sent_features), location)
-	tweets_sentiment[tweet_id] = curr_tweet_info
+	if nb_classifier.classify(test_sent_features) == "pos":
+		pos += 1
+	else:
+		neg += 1
+
 
 # 9) store the class and the tweets and whatever else - should all be in that dictionary
 	#-don't actually need to do anything in this step since it is in dictionary
 # 10) determine the overall/average class of the tweets in that area
-#- dictionary to map location to count of positive and negative
-location_data = {}
-for tweet, info in tweets_sentiment.iteritems():
-	if (location_data.get(info.loc) == None): #default the location to empty dictionary
-		location_data[info.loc] = {}
-	#get the sentiment for the tweet in that location and increment its count  - if it hasn't been seen before default its value to 0 then still increment
-	location_data[info.loc][info.sent] = location_data[info.loc].get(info.sent, 0) + 1
-#???DID WE NEED TO DO SOMETHING WITH POINTWISE MUTUAL INFORMATION HERE OR BEFORE???
+
 
 #- !!! can just determine which class for location is better by doing comparison on the front end side or can do before hand !!!
 # 11) pass the location data dictionary to the front end to be displayed 
