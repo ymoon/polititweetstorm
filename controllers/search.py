e8ee3f1b53ccb8ac84e90e800e1f1db46fdad58d
Mathew Wiesman <mfwiesman@gmail.com>
2016-04-10 11:58:10 -0400

    should fix up controller and added blank html page that should be the one rendered - not sure if we have to change or add blueprint stuff

--- controllers/search.py
+++ controllers/search.py
@@ -11,17 +11,17 @@ search = Blueprint('search', __name__, template_folder='templates')
 @search.route('/search')
 def search_route():
 
-	# if topic: #requests/search has been sent - run it and load the results page
-	# 	topic = request.args.get("topic")
-	# 	location = request.args.get("location")
+	topic = request.args.get("topic")
+	location = request.args.get("location")
+	if topic and location: #requests/search has been sent - run it and load the results page
+		
+		search_results = gt.grab_tweets(topic, location)
+		sentiment_results = nt.sent_system(search_results)
 
-	# 	search_results = gt.grab_tweets(topic, location)
-	# 	sentiment_results = nt.sent_system(search_results)
 
-
-	# 	return render_template("results.html", sentiment_results = sentiment_results)
-	# if: #the normal search page
-	#display trending topics
-	topics = ts.get_top_topics()
+		return render_template("results.html", sentiment_results = sentiment_results)
+	else: #the normal search page
+		#display trending topics
+		topics = ts.get_top_topics()
 
 	return render_template("search.html", initialize=True, topics = topics)
