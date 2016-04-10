97465f6b780c725fe486a448679b7a5ca1b21667
John Moon <ymoon95@gmail.com>
2016-04-10 16:01:55 -0400

    Adding results page

--- controllers/main.py
+++ controllers/main.py
@@ -5,11 +5,4 @@ main = Blueprint('main', __name__, template_folder='templates')
 
 @main.route('/')
 def main_route():
-    # trending = get_trending_data()
     return render_template("index.html")
-
-# def get_trending_data():
-# 	cur = mysql.cursor()
-# 	cur.execute('SELECT topic FROM polititweetstorm.Trending')
-# 	trending = cur.fetchall()
-# 	return trending
