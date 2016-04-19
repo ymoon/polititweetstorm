965822adffd6fed98a6f664f61b5dfa4371490ca
Mat Wiesman <mfwiesman@Mathews-MacBook-Pro-737.local>
2016-04-19 13:07:11 -0400

    code and comment clean up

--- classifier/training.py
+++ classifier/training.py
@@ -1,5 +1,4 @@
 import pandas
-#from nltk.classify import NaiveBayesClassifier as NBC
 
 from nltk.tokenize import word_tokenize
 import nltk
@@ -12,9 +11,11 @@ sys.setdefaultencoding('utf8')
 
 input = "sad.csv"
 
+# Dictionaries to store information from training - with examples
+
 #dictionary mapping the number of positive tweets that appear in data and the number of negative tweets
 #eg. {positive: 4992000, negative: 423482340203}
-class_probs ={}
+class_probs = {}
 #dictionary mapping the size (i.e. how many words (not unique) per positive and negative)
 #eg. {positive: 209348209348129348, negative: 2039482093481234}
 cateogry_size = {}
@@ -22,10 +23,9 @@ cateogry_size = {}
 #eg {positive: {hey: 4, how: 5, is: 6, your_mom: 80000, negative: {lolz: 234}}
 word_probs = {}
 
-
 count = 0;
 
-#1). Loop through training data to get tweets and their sentiments
+#1) Loop through training data to get tweets and their sentiments
 for df in pandas.read_csv(input, chunksize=1):
     count += 1
     print count
@@ -33,24 +33,24 @@ for df in pandas.read_csv(input, chunksize=1):
         break
     else:
         
-        sent_int = int(df.iloc[0]['Sentiment'])#0 is negative, 1 is postive
+        sent_int = int(df.iloc[0]['Sentiment']) # 0 is negative, 1 is postive
         if sent_int == 0:
             sent = 'neg'
         else:
             sent = 'pos'
         
-        class_probs[sent] = class_probs.get(sent, 0) + 1#add to class frequency dictionary
+        class_probs[sent] = class_probs.get(sent, 0) + 1 # Add to class frequency dictionary
         
-        text = str(df.iloc[0]['SentimentText']) #grabbing actual twitter text from the SentimentSource column
+        text = str(df.iloc[0]['SentimentText']) # Grabbing actual twitter text from the SentimentText column
 
         try:
-            tokens = word_tokenize(text) #tokenize the tweet text
-        except UnicodeDecodeError: #catch decode errors 
+            tokens = word_tokenize(text) # Tokenize the tweet text
+        except UnicodeDecodeError: # Catch decode errors and skip over them
             continue
 
-        cateogry_size[sent] = cateogry_size.get(sent, 0) + len(tokens) #add to total word count dictionary
+        cateogry_size[sent] = cateogry_size.get(sent, 0) + len(tokens) # Add to total word count dictionary
 
-        #for each token in your token list, add frequency to dictionary of word probs for that category
+        # For each token in your token list, add frequency to dictionary of word probs for that category
         for t in tokens:
             lower_t = t.lower()
             word_probs[sent] = word_probs.get(sent, {})
@@ -64,7 +64,7 @@ dictionaries.append(class_probs)
 dictionaries.append(cateogry_size)
 dictionaries.append(word_probs)
 
-
+# Output trained system dictionaries to a JSON file for easy read in and use in later classification
 with open("training_information.json", 'w') as train:
     json.dump(dictionaries, train)
 
--- controllers/results.py
+++ controllers/results.py
@@ -1,9 +1,9 @@
-from flask import *
-import classifier.topic_search as ts
+# from flask import *
+# import classifier.topic_search as ts
 
-results = Blueprint('results', __name__, template_folder='templates')
+# results = Blueprint('results', __name__, template_folder='templates')
 
-@results.route('/results')
-def search_route():
-	topics = ts.get_top_topics()
-	return render_template("results.html", topics = topics)
+# @results.route('/results')
+# def search_route():
+# 	topics = ts.get_top_topics()
+# 	return render_template("results.html", topics = topics)
