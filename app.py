0ccd3a24c5507d8a33fe413ac44fe0499cc6f8fc
Mat Wiesman <mfwiesman@gmail.com>
2016-05-04 18:32:51 -0500

    added sessions to handle storing topic for 1 day - added button to refresh topics earlier - next step is to integrate the alchemy into the actual display interface

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+from datetime import timedelta
 
 # from extensions import mysql
 import controllers
@@ -9,6 +10,9 @@ app = Flask(__name__, template_folder='templates')
 # Register the controllers
 app.register_blueprint(controllers.main, url_prefix='/polititweetstorm', static_url_path="static")
 app.register_blueprint(controllers.search, url_prefix='/polititweetstorm', static_url_path="static")
+
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
 
 # Listen on external IPs
 # For us, listen to port 3000 so you can just run 'python app.py' to start the server
--- controllers/search.py
+++ controllers/search.py
@@ -24,41 +24,53 @@ def get_geocor(city):
 def search_route():
 	
 	if request.method == "POST":
-		print "test"
-		text_to_info = {}
-		topic = request.form.get("topic")
-		location_name = request.form.get("location")
-		print location_name
-		print topic
-		relevent_tweets = set()
-		if topic and location_name: # Requests/search has been sent - run it and load the results page
-			location = get_geocor(location_name) # Get latitude and longitude of city
-			print location
-			search_results = gt.grab_tweets(topic, location)
-			for x in search_results["statuses"]:
-				sentence = x["text"]
-				text_to_info[sentence] = (sentence, x["created_at"], x["user"]["screen_name"])
-				relevent_tweets.add(sentence)
-			# if it is a bigram
-			topic = topic.split()
-			if len(topic) > 1:
-				search_results2 = gt.grab_tweets(topic[0], location)
-				for x in search_results2["statuses"]:
+		# Refresh topics by clearing the session
+		if request.form.get("op") == "topic_refresh":
+			session.clear()
+			print "session cleared"
+			return redirect(url_for('search.search_route'))
+		else:
+			text_to_info = {}
+			topic = request.form.get("topic")
+			location_name = request.form.get("location")
+			print location_name
+			print topic
+			relevent_tweets = set()
+			if topic and location_name: # Requests/search has been sent - run it and load the results page
+				location = get_geocor(location_name) # Get latitude and longitude of city
+				print location
+				search_results = gt.grab_tweets(topic, location)
+				for x in search_results["statuses"]:
 					sentence = x["text"]
 					text_to_info[sentence] = (sentence, x["created_at"], x["user"]["screen_name"])
 					relevent_tweets.add(sentence)
+				# if it is a bigram
+				topic = topic.split()
+				if len(topic) > 1:
+					search_results2 = gt.grab_tweets(topic[0], location)
+					for x in search_results2["statuses"]:
+						sentence = x["text"]
+						text_to_info[sentence] = (sentence, x["created_at"], x["user"]["screen_name"])
+						relevent_tweets.add(sentence)
 
-				search_results3 = gt.grab_tweets(topic[1], location)
-				for x in search_results3["statuses"]:
-					sentence = x["text"]
-					text_to_info[sentence] = (sentence, x["created_at"], x["user"]["screen_name"])
-					relevent_tweets.add(sentence)
+					search_results3 = gt.grab_tweets(topic[1], location)
+					for x in search_results3["statuses"]:
+						sentence = x["text"]
+						text_to_info[sentence] = (sentence, x["created_at"], x["user"]["screen_name"])
+						relevent_tweets.add(sentence)
 
-		sentiment_results = ns.sent_system(relevent_tweets, text_to_info)
-		alc_sentiment_results = alcS.alc_sent_system(relevent_tweets, text_to_info, topic)
-		
-		return render_template("results.html", sentiment_results = sentiment_results, topic = topic, location = location_name)
+			sentiment_results = ns.sent_system(relevent_tweets, text_to_info)
+			alc_sentiment_results = alcS.alc_sent_system(relevent_tweets, text_to_info, topic)
+			
+			return render_template("results.html", sentiment_results=sentiment_results, topic=topic, location=location_name)
 	else: # Display trending topics - The normal search page 
-		topics = ts.get_top_topics()
-		return render_template("search.html", initialize=True, topics = topics)
+		# Check if topics have been stored in cookies/already retrieved for the day
+		if 'topics' not in session:
+			print "retrieving topics"
+			topics = ts.get_top_topics()
+			session['topics'] = topics
+		else:
+			print "session active"
+			topics = session['topics']
+		return render_template("search.html", initialize=True, topics=topics)
 
